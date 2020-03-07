7fc93039b46b6c6e28e760cc55a88754e91144c9
zhuoli liang <zhuoliseattle@gmail.com>
2020-03-07 08:50:38 -0800

    retrieveSingleKeyVaultAttributes

--- listkv.py
+++ listkv.py
@@ -3,24 +3,25 @@ from azure.common.credentials import ServicePrincipalCredentials
 from azure.keyvault.secrets import SecretClient
 import os
 
-credentials = ServicePrincipalCredentials(
-    client_id = os.getenv('AZURE_CLIENT_ID'),
-    secret = os.getenv('AZURE_CLIENT_SECRET'),
-    tenant = os.environ.get('AZURE_CLIENT_TENANT')
-)
+def retrieveSingleKeyVaultAttributes(vault_url, resourceName, version, credentials):
+    client = KeyVaultClient(credentials)
+    secretBundle = client.get_secret(vault_url, resourceName, version)
+    return secretBundle.attributes
 
-client = KeyVaultClient(credentials)
+def main():
+    credentials = ServicePrincipalCredentials(
+        client_id = os.getenv('AZURE_CLIENT_ID'),
+        secret = os.getenv('AZURE_CLIENT_SECRET'),
+        tenant = os.environ.get('AZURE_CLIENT_TENANT')
+    )
+    VAULT_URL="https://zhuolikeyvaultmonitor.vault.azure.net/"
+    resourceName="ZhuoliSecretExpire"
+    version="930d7bdf9ff448d4b61d2974c2d2940c"
+    # VAULT_URL must be in the format 'https://<vaultname>.vault.azure.net'
+    # KEY_VERSION is required, and can be obtained with the KeyVaultClient.get_key_versions(self, vault_url, key_name) API
+    print(retrieveSingleKeyVaultAttributes(VAULT_URL,resourceName,version,credentials))
 
-VAULT_URL="https://zhuolikeyvaultmonitor.vault.azure.net/"
-# VAULT_URL must be in the format 'https://<vaultname>.vault.azure.net'
-# KEY_VERSION is required, and can be obtained with the KeyVaultClient.get_key_versions(self, vault_url, key_name) API
-secretBundle = client.get_secret(VAULT_URL, "ZhuoliSecretExpire", "930d7bdf9ff448d4b61d2974c2d2940c")
-print(secretBundle.attributes)
-
-# secrets = secret_client.list_properties_of_secrets()
-# secret_client = SecretClient(vault_url, credentials)
-# for secret in secrets:
-#     # the list doesn't include values or versions of the secrets
-#     print(secret.id)
-#     print(secret.name)
-#     print(secret.enabled)
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+    
--- main.py
+++ main.py
@@ -1,5 +1,34 @@
 import requests
+from azure.keyvault import KeyVaultClient
+from azure.common.credentials import ServicePrincipalCredentials
+from azure.keyvault.secrets import SecretClient
+import os
 
 response = requests.get('https://httpbin.org/ip')
 
-print('Your IP is {0}'.format(response.json()['origin']))
+def getIP():
+    print('Your IP is {0}'.format(response.json()['origin']))
+
+def retrieveSingleKeyVaultAttributes(vault_url, resourceName, version, credentials):
+    client = KeyVaultClient(credentials)
+    secretBundle = client.get_secret(vault_url, resourceName, version)
+    return secretBundle.attributes
+
+def main():
+    credentials = ServicePrincipalCredentials(
+        client_id = os.getenv('AZURE_CLIENT_ID'),
+        secret = os.getenv('AZURE_CLIENT_SECRET'),
+        tenant = os.environ.get('AZURE_CLIENT_TENANT')
+    )
+    client = KeyVaultClient(credentials)
+    VAULT_URL="https://zhuolikeyvaultmonitor.vault.azure.net/"
+    resourceName="ZhuoliSecretExpire"
+    version="930d7bdf9ff448d4b61d2974c2d2940c"
+    # VAULT_URL must be in the format 'https://<vaultname>.vault.azure.net'
+    # KEY_VERSION is required, and can be obtained with the KeyVaultClient.get_key_versions(self, vault_url, key_name) API
+    print(retrieveSingleKeyVaultAttributes(VAULT_URL,version, client)
+    
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
